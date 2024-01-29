e7e9fc6efd2bc553d1168b2143b9ba918042b05f
rouman1201082 <chernovrn@gmail.com>
2024-01-29 18:40:35 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const nameProduct = 'apple';
+const quantityProduct = 10;
+const categoryProduct = 'fruit';
+const pricetProduct = 15;
+
+const summPriceProduct = quantityProduct * pricetProduct;
+console.log(nameProduct);
+console.log('Общая сумма', ' - ', summPriceProduct);
+
